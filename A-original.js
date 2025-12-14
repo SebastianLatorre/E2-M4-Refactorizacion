ec7dfdc5a1fd1fc42f37369f9a1ede2a78dbd0de
SebastianLatorre <seba.latorre.diaz@gmail.com>
2025-12-14 17:46:57 -0300

    Se agrega el archivo original

--- A-original.js
+++ A-original.js
@@ -24,24 +24,3 @@ function crearMensajePresentacion(user) {
 var mensajeDeBienvenida = crearMensajePresentacion(usuario);
 console.log(mensajeDeBienvenida);
 
-//=======================================
-// Código refactorizado ES6+
-//=======================================
-
-// Objeto que representa a un usuario
-const usuario = {
-  nombre: 'Ana',
-  edad: 24,
-  ciudad: 'Barcelona'
-};
-
-// Función flecha para crear el mensaje de presentación con destructuring
-const crearMensajePresentacion = ({ nombre, edad, ciudad }) => {
-  const mensaje = `Hola, mi nombre es ${nombre}, tengo ${edad} años y vivo en la ciudad de ${ciudad}.`;
-  
-  return mensaje;
-};
-
-// Se llama a la función y se muestra el resultado en consola
-const mensajeDeBienvenida = crearMensajePresentacion(usuario);
-console.log(mensajeDeBienvenida);
